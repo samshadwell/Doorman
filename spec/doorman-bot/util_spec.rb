e6f7878b930b7e3f10484d3041c3a0ada12eefd2
Samuel Shadwell <smshdwll@gmail.com>
2017-12-30 16:49:20 -0800

    Fixes rubocop errors.

--- spec/doorman-bot/util_spec.rb
+++ spec/doorman-bot/util_spec.rb
@@ -1,17 +1,14 @@
+# frozen_string_literal: true
+
 require 'doorman-bot/util'
 
 RSpec.describe DoormanBot::Util do
   describe '#guest_lists' do
     it 'properly processes files' do
-      expect(Dir).to receive(:entries).and_return([
-        '.',
-        '..',
-        'foo',
-        'bar.yml',
-        'a.yml',
-        'cat.yml',
-      ])
-      expect(described_class.instance.guest_lists).to eq(['a', 'bar', 'cat'])
+      expect(Dir).to receive(:entries).and_return(
+        %w[. .. foo bar.yml a.yml cat.yml]
+      )
+      expect(described_class.instance.guest_lists).to eq(%w[a bar cat])
     end
   end
 end
