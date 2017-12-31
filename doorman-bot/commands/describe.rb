97b8ebf5aca0666b85e9cf1006e71eb0576b7787
Samuel Shadwell <smshdwll@gmail.com>
2017-12-31 10:44:53 -0800

    Adds help command text.

--- doorman-bot/commands/describe.rb
+++ doorman-bot/commands/describe.rb
@@ -5,8 +5,12 @@ module DoormanBot
     # List command, fetches all groups known groups and sends to user.
     class Describe < SlackRubyBot::Commands::Base
       def self.call(client, data, _match)
-        client.say(channel: data.channel,
-                   text: 'Describe this boiii')
+        client.say(
+          channel: data.channel,
+          text: "I'm sorry, you need to provide a guest list to describe. " \
+                'For more information try `doorman help describe`',
+          thread_ts: data.thread_ts || data.ts
+        )
       end
     end
   end
--- doorman-bot/doorman.rb
+++ doorman-bot/doorman.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 module DoormanBot
+  # Base SlackBot.
   class Doorman < SlackRubyBot::Bot
+    help do
+      title 'Doorman'
+      desc 'Simplifies multi-channel invitations.'
+
+      command 'list' do
+        desc 'Lists all available "guest lists" a user can be invited to.'
+        long_desc 'Command format: doorman list'
+      end
+
+      command 'describe' do
+        desc 'Lists all the channels included in the given guest list.'
+        long_desc 'Command format: doorman describe <guest list>'
+      end
+    end
   end
 end
