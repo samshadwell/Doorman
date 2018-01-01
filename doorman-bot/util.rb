9566aa657d9b630b9a2d607acdfd96e47c1b60c0
Samuel Shadwell <smshdwll@gmail.com>
2018-01-01 11:41:49 -0800

    Clean up util.rb

--- doorman-bot/util.rb
+++ doorman-bot/util.rb
@@ -9,47 +9,8 @@ module DoormanBot
   class Util
     include Singleton
 
-    # @channels is a hash mapping channel names to their IDs
-    attr_reader :channels
-
     INHERIT_KEY = 'inherit-from'
     INCLUDE_KEY = 'include-channels'
-
-    def initialize
-      refresh_channels!
-    end
-
-    def guest_lists
-      # Memoize guest lists since these cannot change unless app is redeployed.
-      @guest_lists ||=
-        Dir.entries(DoormanBot::GUESTS_DIRECTORY)
-           .select { |filename| filename.end_with? '.yml' }
-           .map { |filename| filename.chomp('.yml') }
-           .sort
-    end
-
-    # The Slack API doesn't support looking up channels by name and we need the
-    # IDs to send invitations. Cache the results of getting all channels and
-    # groups (private channels) to minimize the number of times we fetch these
-    # from Slack.
-    def refresh_channels!
-      client = ::Slack::Web::Client.new
-      all_channels = client.channels_list.channels
-      all_channels += client.groups_list.groups
-      name_to_id = {}
-      all_channels.each do |c|
-        name_to_id[c.name] = c.id
-      end
-
-      @channels = name_to_id.freeze
-    end
-
-    # Given the name of some guest list, returns a list of channel IDs that are
-    # included by that guest list.
-    def get_target_ids(list_name)
-      names = get_target_names(list_name)
-      translate_names_to_ids(names)
-    end
 
     # Given the name of some guest list, returns a list of channel names that
     # are included by that guest list.
@@ -71,7 +32,51 @@ module DoormanBot
       channel_names.to_a.sort
     end
 
+    def guest_lists
+      # Memoize guest lists since these cannot change unless app is redeployed.
+      @guest_lists ||=
+        Dir.entries(DoormanBot::GUESTS_DIRECTORY)
+           .select { |filename| filename.end_with? '.yml' }
+           .map { |filename| filename.chomp('.yml') }
+           .sort
+    end
+
     private
+
+    def initialize
+      @channels = {}.freeze
+      @group_members = {}
+      refresh_channels!
+    end
+
+    # The Slack API doesn't support looking up channels by name and we need the
+    # IDs to send invitations. Cache the results of getting all channels and
+    # groups (private channels) to minimize the number of times we fetch these
+    # from Slack.
+    def refresh_channels!
+      client = ::Slack::Web::Client.new
+      all_groups = client.groups_list.groups
+      all_groups.each { |g| @group_members[g.id] = g.members }
+
+      all_channels = client.channels_list.channels
+      name_to_id = {}
+      (all_channels + all_groups).each do |c|
+        name_to_id[c.name] = c.id
+      end
+
+      @channels = name_to_id.freeze
+    end
+
+    # Given the name of some guest list, returns a list of channel IDs that are
+    # included by that guest list.
+    def get_target_ids(list_name)
+      names = get_target_names(list_name)
+      translate_names_to_ids(names)
+    end
+
+    def can_invite_to_group?(current_user, group_id)
+      @group_members[group_id].include?(current_user)
+    end
 
     def parse_guest_list(list_name)
       filename = "#{DoormanBot::GUESTS_DIRECTORY}/#{list_name}.yml"
